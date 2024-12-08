c84d8ca058b7b095c23aa5da2d38b2cc8e251f47
Van Dao <dao28901@gmail.com>
2024-12-08 08:29:56 +0700

    viet cors cho backend

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -46,13 +46,19 @@ Future<Response> _submitHandler(Request request) async {
     final data = json.decode(payload); //tra ve 1 map
     //lay gia tri name tu data, ep ve string neu co the
     final name = data['name'] as String?;
+    final num = int.parse(data['num']);
+
     //kiem tra neu name hop le
-    if (name != null && name.isNotEmpty) {
+    if (name != null &&
+        name.isNotEmpty &&
+        0 <= num &&
+        num <= 10 &&
+        num != null) {
       //phan hoi chao mung
-      final response = {"message": 'Chao mung ${name}'};
+      final response = {"message": 'Chao mung ${name} ${num}'};
       return Response.ok(json.encode(response), headers: _headers);
     } else {
-      final response = {"message": 'Hay nhap ten cua ban'};
+      final response = {"message": 'Hay nhap ten va diem (0->10) cua ban '};
       return Response.badRequest(
           body: json.encode(response), headers: _headers);
     }
@@ -63,12 +69,31 @@ Future<Response> _submitHandler(Request request) async {
 }
 
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
+  // lắng nghe trên tất cả các địa chỉ IPv4
   final ip = InternetAddress.anyIPv4;
 
+  final corsHeader = createMiddleware(requestHandler: (req) {
+    if (req.method == 'OPTIONS') {
+      return Response.ok("", headers: {
+        // cho phep moi nguon truy cap trong moi truong dev
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH,HEAD',
+        'Access-Control-Allow-Headers': "Content-Type,Authorization",
+      });
+    }
+    return null;
+  }, responseHandler: (res) {
+    return res.change(headers: {
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH,HEAD',
+      'Access-Control-Allow-Headers': "Content-Type,Authorization",
+    });
+  });
   // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final handler = Pipeline()
+      .addMiddleware(corsHeader)
+      .addMiddleware(logRequests())
+      .addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
